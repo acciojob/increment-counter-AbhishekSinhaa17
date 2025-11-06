2171c8be597408832f08e94327f156c52c32c774
Abhishek Kumar Sinha <165494965+AbhishekSinhaa17@users.noreply.github.com>
2025-11-06 17:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ const counterElement = document.getElementById("counter");
+    const incrementButton = document.getElementById("incrementBtn");
+
+    let count = 0;
+
+    incrementButton.addEventListener("click", function() {
+      alert(count);
+
+      count++;
+
+      counterElement.textContent = count;
+    });
